5a224ccc9384ecf757f9d975d546e6c966e2a7a0
lingan233 <71675451+lingan233@users.noreply.github.com>
2022-12-07 10:22:36 -0500

    still debugging live 2

--- src/features/user_cart.js
+++ src/features/user_cart.js
@@ -12,13 +12,13 @@ import { createSlice } from "@reduxjs/toolkit";
 
 // const initialStateValue = getValue();
 
-
+// console.log(localStorage.getItem("products"))
 
 const initialStateValue = [
-  ...JSON.parse(localStorage.getItem("products"))].map((product) => ({
+  ...JSON.parse(localStorage.getItem("products")).map((product) => ({
   ...product,
   in_cart: 0,
-}));
+}))];
 
 export const redux_productSlice = createSlice({
   name: "redux_user_cart",
